cb6f0a96ca92096cbbdad6fff2b00102843cb4ee
Yehor <egorcherkas@gmail.com>
2023-03-10 12:03:39 +0100

    start

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryCardMarkUp(items) {
+return items.map(({ preview, original, description}) => {
+return`
+<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}).join('');
+};
+
+const cardsMarkup = createGalleryCardMarkUp(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryContainer.addEventListener('click', handleGalleryClick);
+
+function handleGalleryClick(event) {
+    event.preventDefault();
+
+    if(event.target.nodeName !== 'IMG') {
+        return
+    }
+    const modalImg = event.target.dataset.source
+    console.log(modalImg);
+
+    const instance = basicLightbox.create(`<img src= "${modalImg}" alt="" width="800" height="600">`)
+    instance.show();
+};
+
+
+// console.log(galleryItems);
